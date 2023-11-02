ebad9732e8b250eceb845c0a8c60e610437a3524
doronz <doron88@gmail.com>
2023-11-02 20:39:59 +0200

    ServiceConnection: remove deprecated `create()` method

--- pymobiledevice3/service_connection.py
+++ pymobiledevice3/service_connection.py
@@ -5,7 +5,6 @@ import socket
 import ssl
 import struct
 import time
-from enum import Enum, auto
 from typing import Mapping, Optional
 
 # from pygments import formatters, highlight, lexers
@@ -13,6 +12,11 @@ from typing import Mapping, Optional
 from pymobiledevice3.exceptions import ConnectionFailedError, ConnectionTerminatedError, NoDeviceConnectedError, \
     PyMobileDevice3Exception
 from pymobiledevice3.usbmux import MuxDevice, select_device
+from pymobiledevice3.utils import set_keepalive
+
+DEFAULT_AFTER_IDLE_SEC = 3
+DEFAULT_INTERVAL_SEC = 3
+DEFAULT_MAX_FAILS = 3
 
 SHELL_USAGE = """
 # This shell allows you to communicate directly with every service layer behind the lockdownd daemon.
@@ -56,11 +60,6 @@ def create_context(certfile, keyfile=None):
     return context
 
 
-class Medium(Enum):
-    TCP = auto()
-    USBMUX = auto()
-
-
 class LockdownServiceConnection:
     """ wrapper for usbmux tcp-relay connections """
 
@@ -88,13 +87,6 @@ class LockdownServiceConnection:
             raise NoDeviceConnectedError()
         sock = target_device.connect(port)
         return LockdownServiceConnection(sock, mux_device=target_device)
-
-    @staticmethod
-    def create(medium: Medium, identifier: str, port: int, connection_type: str = None) -> 'LockdownServiceConnection':
-        if medium == Medium.TCP:
-            return LockdownServiceConnection.create_using_tcp(identifier, port)
-        else:
-            return LockdownServiceConnection.create_using_usbmux(identifier, port, connection_type=connection_type)
 
     def setblocking(self, blocking: bool) -> None:
         self.socket.setblocking(blocking)
